09b6750f0d38d3edf3d14d2943de6539dfe5effc
Alice Lieutier <alice.lieutier@gmail.com>
2013-04-27 17:28:06 +0100

    now using requestAnimationFrame for a smoother experience with less lumps

--- smoothscroll.js
+++ smoothscroll.js
@@ -15,7 +15,7 @@ var getTop = function(element) {
 // http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
 var easeInOutCubic = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
 
-// calculate the scroll position we should be in 
+// calculate the scroll position we should be in
 // given the start and end point of the scroll
 // the time elapsed from the beginning of the scroll
 // and the total time the scroll should last (default 500ms)
@@ -25,36 +25,43 @@ var position = function(start, end, elapsed, time) {
     // return start + (end - start) * (elapsed / time); // <-- this would give a linear scroll
 }
 
-// We cannot rely on setInterval to execute a function at a regular interval
-// The idea is to recalculate the position we should be at and scroll there directly
-// every more or less 10ms.
+// we use requestAnimationFrame to be called by the browser before every repaint
 // if the callback exist, it is called when the scrolling is finished
 var smoothScroll = function(el, time, callback){
     time = time || 2000;
     var start = window.pageYOffset;
     var end = getTop(el);
     var clock = Date.now();
-    var j = window.setInterval(function(){
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
+        function(fn){window.setTimeout(fn, 15);};
+
+    var step = function(){
         var elapsed = Date.now() - clock;
         window.scroll(0, position(start, end, elapsed, time));
         if (elapsed > time) {
-            window.clearInterval(j);
             if (typeof callback === 'function') {
                 callback(el);
             }
+        } else {
+            requestAnimationFrame(step);
         }
-    }, 10);
+    }
+    step();
 }
 
 var linkHandler = function(ev) {
     ev.preventDefault();
     var hash = this.href.split('#').pop();
     window.history.pushState(null, null, '#' + hash)
-    //using the history api to solve issue #1 - back doesn't work 
-    // change the srolling time in this call
+    // using the history api to solve issue #1 - back doesn't work
+    // most browser don't update :target when the history api is used:
+    // THIS IS A BUG FROM THE BROWSERS.
+    // change the scrolling time in this call
     smoothScroll(document.getElementById(hash), 500, function(el) {
         window.location.hash = el.id;
-        // necessary if we want to use the :target selector.
+        // in Opera and Firefox, this will cause the :target to be activated.
+        // In Chrome, no solution yet.
         // the browser does not push a second state because the url hasn't changed.
     });
 }
