2b6c08f4b7ab2eca0f68402afb484d811daca6be
Philipp B <philipp.b@ya.ru>
2017-04-07 13:26:56 +0300

    Correct buffer behaviour
    … on default internal link matching.

--- smoothscroll.js
+++ smoothscroll.js
@@ -20,38 +20,40 @@
 'use strict';
 
 // Do not initialize smoothScroll when running server side, handle it in client:
-if (typeof window !== 'object') return;
+if (typeof window !== 'object') { return; }
 
 // We do not want this script to be applied in browsers that do not support those
 // That means no smoothscroll on IE9 and below.
-if(document.querySelectorAll === void 0 || window.pageYOffset === void 0 || history.pushState === void 0) { return; }
+if (document.querySelectorAll === void 0 || window.pageYOffset === void 0 || history.pushState === void 0) { return; }
 
 // Get the top position of an element in the document
 var getTop = function(element, start) {
 		// return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-		if(element.nodeName === 'HTML') return -start
-		return element.getBoundingClientRect().top + start
-}
+		if (element.nodeName === 'HTML') { return -start; }
+		else { return element.getBoundingClientRect().top + start; }
+};
 // ease in out function thanks to:
 // http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
-var easeInOutCubic = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+var easeInOutCubic = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 };
 
 // calculate the scroll position we should be in
 // given the start and end point of the scroll
 // the time elapsed from the beginning of the scroll
 // and the total duration of the scroll (default 500ms)
 var position = function(start, end, elapsed, duration) {
-		if (elapsed > duration) return end;
-		return start + (end - start) * easeInOutCubic(elapsed / duration); // <-- you can change the easing funtion there
-		// return start + (end - start) * (elapsed / duration); // <-- this would give a linear scroll
-}
+		if (elapsed > duration) { return end; }
+		else {
+			return start + (end - start) * easeInOutCubic(elapsed / duration); // <-- you can change the easing funtion there
+			// return start + (end - start) * (elapsed / duration); // <-- this would give a linear scroll
+		}
+};
 
 // we use requestAnimationFrame to be called by the browser before every repaint
 // if the first argument is an element then scroll to the top of this element
 // if the first argument is numeric then scroll to this location
 // if the callback exist, it is called when the scrolling is finished
 // if context is set then scroll that element, else scroll window
-var smoothScroll = function(el, duration, buffer, callback, context){
+var smoothScroll = function (el, duration, buffer, callback, context) {
 		duration = duration || 500;
 		context = context || window;
 		var start = ( context.scrollTop || window.pageYOffset ) + buffer;
@@ -90,16 +92,18 @@ var smoothScroll = function(el, duration, buffer, callback, context){
 var linkHandler = function(ev) {
 		ev.preventDefault();
 
-		if (location.hash !== this.hash) window.history.pushState(null, null, this.hash)
+		if (location.hash !== this.hash) {
+			window.history.pushState(null, null, this.hash);
+		}
 		// using the history api to solve issue #1 - back doesn't work
 		// most browser don't update :target when the history api is used:
 		// THIS IS A BUG FROM THE BROWSERS.
 		// change the scrolling duration in this call
-		var node = document.getElementById(this.hash.substring(1))
-		if(!node) return; // Do not scroll to non-existing node
+		var node = document.getElementById(this.hash.substring(1));
+		if(!node) { return; } // Do not scroll to non-existing node
 
-		smoothScroll(node, 500, function(el) {
-				location.replace('#' + el.id)
+		smoothScroll(node, 500, 0, function(el) {
+				location.replace('#' + el.id);
 				// this will cause the :target to be activated.
 		});
 }
