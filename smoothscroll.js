776228f875c16d70f22a02a5f2f174cb3f4c9032
Leonid Fenko <fenok2112@gmail.com>
2017-10-12 23:06:52 +0300

    Makes it possible to preventDefault() on link click event

--- smoothscroll.js
+++ smoothscroll.js
@@ -88,20 +88,22 @@ var smoothScroll = function(el, duration, callback, context){
 }
 
 var linkHandler = function(ev) {
-    ev.preventDefault();
+    if (!ev.defaultPrevented) {
+        ev.preventDefault();
 
-    if (location.hash !== this.hash) window.history.pushState(null, null, this.hash)
-    // using the history api to solve issue #1 - back doesn't work
-    // most browser don't update :target when the history api is used:
-    // THIS IS A BUG FROM THE BROWSERS.
-    // change the scrolling duration in this call
-    var node = document.getElementById(this.hash.substring(1))
-    if(!node) return; // Do not scroll to non-existing node
+        if (location.hash !== this.hash) window.history.pushState(null, null, this.hash)
+        // using the history api to solve issue #1 - back doesn't work
+        // most browser don't update :target when the history api is used:
+        // THIS IS A BUG FROM THE BROWSERS.
+        // change the scrolling duration in this call
+        var node = document.getElementById(this.hash.substring(1))
+        if (!node) return; // Do not scroll to non-existing node
 
-    smoothScroll(node, 500, function(el) {
-        location.replace('#' + el.id)
-        // this will cause the :target to be activated.
-    });
+        smoothScroll(node, 500, function (el) {
+            location.replace('#' + el.id)
+            // this will cause the :target to be activated.
+        });
+    }
 }
 
 // We look for all the internal links in the documents and attach the smoothscroll function
